57539a0474cfb96e32f0f70b73b9d1956c3f201c
Paulo Galdo <paulo.galdo@incluit.com>
2022-09-29 18:14:15 -0300

    Heroku

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express'),
     app = express();
 
-app.use(express.static('./dist/portal-galdo'));
+app.use(express.static('./dist/capacitacion'));
 
 app.get('/*', function (req, res) {
-    res.sendFile('index.html', { root: 'dist/portal-galdo/' });
+    res.sendFile('index.html', { root: 'dist/capacitacion/' });
 });
 
 app.listen(process.env.PORT || 8080);
